053529983b0ef0a4871a92d687b552f3cef80559
Billy Arnold <billyarnoldd@gmail.com>
2024-03-27 20:34:58 +0000

    Messing with snake class

--- script.js
+++ script.js
@@ -14,7 +14,8 @@ class Board {
     this.drawGrid();
   }
 
-  drawGrid() {
+  drawGrid(snakeBody) {
+    console.log("draw grid", snakeBody);
     for (let i = 0; i <= this.rows; i++) {
       this.context.moveTo(0, i * this.cellSize);
       this.context.lineTo(this.canvas.width, i * this.cellSize);
@@ -27,7 +28,52 @@ class Board {
 
     this.context.strokeStyle = "black";
     this.context.stroke();
+
+    this.context.fillStyle = "green";
+    snakeBody.forEach((segment) => {
+      this.context.fillRect(
+        segment.x * this.cellSize,
+        segment.y * this.cellSize,
+        this.cellSize,
+        this.cellSize,
+      );
+    });
   }
 }
 
-const board = new Board("myCanvas");
+//snake class
+//generate a snake with length
+//track length
+//increase length using food collision
+//check collisions
+//move snake in current direction
+//track current directions and change current directions using arrows
+class Snake {
+  constructor() {
+    this.body = [{ x: 10, y: 10 }];
+  }
+
+  getBody() {
+    return this.body;
+  }
+}
+
+//game class
+class SnakeGame {
+  constructor() {
+    this.board = new Board("myCanvas");
+    this.snake = new Snake();
+    this.interval = setInterval(this.update.bind(this), 100);
+  }
+
+  update() {
+    console.log("updating game");
+    const context = this.board.context;
+    this.board.drawGrid();
+  }
+}
+
+new SnakeGame();
+
+//food class
+//generate random food item without colliding
